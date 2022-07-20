d9fe60fb4ceb0fa4b5cad0046f86542228dd94c0
dmalinovsk01 <107026577+dmalinovsk01@users.noreply.github.com>
2022-07-20 21:44:07 +0300

    initials

---
+++ js/js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//const result = confirm("are you here?");
+//console.log(result);
+
+// const answer = +prompt("are you 18?", "18");
+// //console.log(typeof(answer));
+// console.log(answer + 5);
+
+const answers = [];
+
+// answers[0] = prompt('how are you?', '');
+// answers[1] = prompt('how are yo?', '');
+// answers[2] = prompt('how are y?', '');
+
+console.log(typeof(answers));
---
+++ js/js/script1.js
@@ -0,0 +1,29 @@
+"!use sctrict";
+
+
+let number = 4.6;
+
+console.log(-4/0);
+console.log('string * 9');
+
+
+const persone = `5`;
+
+const bool = false;
+
+// console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried : false
+};
+
+console.log(obj.age);
+// console.log(obj["age"]);
+
+let arr = ['plum.png', 'orange.png', 'apple.png', []];
+console.log(arr[1]);
